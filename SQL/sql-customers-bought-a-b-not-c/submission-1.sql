4f87e9cf9769e9f09eadef8c8bd792415b4b270e
Tilak-Codex <156806496+Tilak-Codex@users.noreply.github.com>
2026-09-14 17:22:07 +0530

    Add: sql-customers-bought-a-b-not-c - submission-1

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-1.sql
@@ -0,0 +1,10 @@
+-- Write your query below
+
+select c.customer_id,c.customer_name from customers c 
+where exists (select 1 from orders o where c.customer_id=o.customer_id
+and o.product_name='A')
+and exists (select 1 from orders o where c.customer_id=o.customer_id
+and o.product_name='B')
+and not exists (select 1 from orders o where c.customer_id=o.customer_id
+and o.product_name='C')
+order by customer_name;
